36fb4506647ada7b40568c3ce3ed976aa62f7821
Allan Ferreira <allanf.gferreira@outlook.com>
2024-10-01 19:38:15 +0000

    Remove deprecated basex, basey matplotlib parameters

--- PySpice/Plot/BodeDiagram.py
+++ PySpice/Plot/BodeDiagram.py
@@ -33,7 +33,7 @@ from matplotlib import pyplot
 
 def bode_diagram_gain(axe, frequency, gain, **kwargs):
 
-    axe.semilogx(frequency, gain, basex=10, **kwargs)
+    axe.semilogx(frequency, gain, **kwargs)
     axe.grid(True)
     axe.grid(True, which='minor')
     axe.set_xlabel("Frequency [Hz]")
@@ -43,7 +43,7 @@ def bode_diagram_gain(axe, frequency, gain, **kwargs):
 
 def bode_diagram_phase(axe, frequency, phase, **kwargs):
 
-    axe.semilogx(frequency, phase, basex=10, **kwargs)
+    axe.semilogx(frequency, phase, **kwargs)
     axe.set_ylim(-math.pi, math.pi)
     axe.grid(True)
     axe.grid(True, which='minor')
