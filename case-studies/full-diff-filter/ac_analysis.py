4c145c92e4194c684a371d7bb72791f45f7c923b
Allan Ferreira <allanf.gferreira@outlook.com>
2024-10-09 13:36:21 +0000

    Compare EBNF parser

--- case-studies/full-diff-filter/ac_analysis.py
+++ case-studies/full-diff-filter/ac_analysis.py
@@ -1,5 +1,10 @@
-from PySpice.Spice.Parser import SpiceParser
+#from PySpice.Spice.Parser import SpiceParser
+from PySpice.Spice.EBNFSpiceParser import SpiceParser
 
-parser = SpiceParser(path="case-studies/full-diff-filter/netlist.sp")
-circuit = parser.build_circuit()
+#parser = SpiceParser(path="case-studies/full-diff-filter/netlist.sp")
+parser = SpiceParser.parse(path="case-studies/full-diff-filter/netlist.sp")
+
+#circuit = parser.build_circuit()
+circuit = parser.build()
+
 print(circuit)
